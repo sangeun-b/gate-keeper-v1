548c6091e300a105ec3a2206b547e9176dc7865b
sangeun-b <baek0007@algonquinlive.com>
2022-03-18 01:54:37 +0900

    env2

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,7 +16,9 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
