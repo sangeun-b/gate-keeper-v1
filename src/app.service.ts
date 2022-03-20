c4ebeed3d1cc9726f6162d777d9fcf83949cf74b
sangeun-b <baek0007@algonquinlive.com>
2022-03-20 11:06:36 +0900

    done

--- src/app.service.ts
+++ src/app.service.ts
@@ -7,11 +7,6 @@ export class AppService {
     @InjectFirebaseAdmin() private readonly firebase: FirebaseAdmin,
   ) {}
   getHello(): string {
-    // console.log(this.firebase);
-    console.log(this.firebase.storage);
-    console.log(process.env.NEXT_PUBLIC_FIRBASE_PROJECT_ID);
-    console.log(process.env.FIRBASE_PRIVATE_KEY);
-    console.log(process.env.FIRBASE_PRIVATE_KEY.replace(/\\n/g, '\n'));
     return 'Hello World!';
   }
 }
