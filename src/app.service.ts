d40bd7e61b86ed51af31f4343d3138b39bdd975e
sangeun-b <baek0007@algonquinlive.com>
2022-03-20 10:56:48 +0900

    f1

--- src/app.service.ts
+++ src/app.service.ts
@@ -10,6 +10,8 @@ export class AppService {
     // console.log(this.firebase);
     console.log(this.firebase.storage);
     console.log(process.env.NEXT_PUBLIC_FIRBASE_PROJECT_ID);
+    console.log(process.env.FIRBASE_PRIVATE_KEY);
+    console.log(process.env.FIRBASE_PRIVATE_KEY.replace(/\\n/g, '\n'));
     return 'Hello World!';
   }
 }
