d0c1beb2576a77a428494ac13773b74812dd9403
sangeun-b <baek0007@algonquinlive.com>
2022-04-03 02:10:43 +0900

    fix guest

--- src/guest/guest.controller.ts
+++ src/guest/guest.controller.ts
@@ -110,15 +110,15 @@ export class GuestController {
   }
   @Delete(':aid/guest/:id')
   async remove(@Param('id') id: number, @Param('aid') aid: number) {
-    const guestFind = await this.guestService.findOne(id);
-    if (guestFind) {
-      try {
-        unlinkSync(`./guests/${aid}/${guestFind.img}`);
-      } catch (err) {
-        console.log(err);
-        return 'Delete failed';
-      }
-    }
+    // const guestFind = await this.guestService.findOne(id);
+    // if (guestFind) {
+    //   try {
+    //     unlinkSync(`./guests/${aid}/${guestFind.img}`);
+    //   } catch (err) {
+    //     console.log(err);
+    //     return 'Delete failed';
+    //   }
+    // }
     this.guestService.remove(id);
     return `guest #${id} Deleted!`;
   }
